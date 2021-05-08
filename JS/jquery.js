a893a9a36ebbf8a4b3b817fe193e1ac9772934ce
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-08 22:01:08 +0900

    modified jquery

--- JS/jquery.js
+++ JS/jquery.js
@@ -1,1 +1,9 @@
-alert($("h1").text("Test"));
+alert($("#testalert").click(function(){
+  swal({
+    title: "test alert is generated",
+    text: "Can you see this?",
+    confirmButtonText: "Yep.",
+    ConfirmButtonColor: "#DD6B55",
+    icon: "success"
+  });
+});
